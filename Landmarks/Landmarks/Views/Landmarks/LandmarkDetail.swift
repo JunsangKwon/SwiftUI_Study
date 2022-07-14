692708335004fbade81cad8314c466516d55490b
권준상 <wnstkdsun97@naver.com>
2022-07-14 16:55:39 +0900

    3-Handling User Input

--- Landmarks/Landmarks/Views/Landmarks/LandmarkDetail.swift
+++ Landmarks/Landmarks/Views/Landmarks/LandmarkDetail.swift
@@ -8,8 +8,13 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    @EnvironmentObject var modelData: ModelData
     var landmark: Landmark
     
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
+
     var body: some View {
         ScrollView {
             MapView(coordinate: landmark.locationCoordinate)
@@ -19,10 +24,13 @@ struct LandmarkDetail: View {
                 .offset(y: -130) // y축 기준 -130 offset
                 .padding(.bottom, -130) // 뷰 하단을 기준으로 -130만큼 패딩 지정
             VStack(alignment: .leading) {
-                Text(landmark.name)
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .foregroundColor(.green)
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.green)
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
                 HStack(alignment: .center) {
                     Text(landmark.park)
                         .fontWeight(.medium)
@@ -48,6 +56,6 @@ struct LandmarkDetail: View {
 
 struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkDetail(landmark: landmarks[0])
+        LandmarkDetail(landmark: ModelData().landmarks[0])
     }
 }
